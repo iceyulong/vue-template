a62d5c1ad4c25cea5353be23ec39138d79619301
ylc <2825736107@qq.com>
2020-06-04 01:59:01 +0800

    elementUI+国际化

--- src/main.js
+++ src/main.js
@@ -4,6 +4,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+import { Button, Select, Option } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+/** 国际化 */
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+import VueI18n from 'vue-i18n'
+locale.use(lang)
+Vue.use(VueI18n)
+
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(Option)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,27 @@
                 <p>年龄:{{ item.age }}</p>
             </li>
             </ul>
+            <el-button @click="visible = true">{{now}}</el-button>
+            <el-select v-model="value" placeholder="请选择">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value">
+              </el-option>
+            </el-select>
         </div>
     </template>
 
 <script>
 export default {
   name: 'Home',
+  data () {
+    return {
+      options: [],
+      value: ''
+    }
+  },
   computed: {
     list () {
       return this.$store.state.list
